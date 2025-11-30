ec9e801310a8cd07d7374d2175f2ce56afb30891
Figueiira <tfigueira800@gmail.com>
2025-11-30 17:30:41 -0400

    Atualizações gerais, telas, layouts funções etc

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from dotenv import load_dotenv
+from flask import flash
 import os
 
 # Carrega variáveis do arquivo .env
@@ -45,6 +46,19 @@ def configuracoes():
         return redirect(url_for('login'))
     return render_template('config.html', usuario=session['usuario'])
 
+@app.route('/op')
+def operacoes():
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    return render_template('operacoes.html', usuario=session['usuario'])
+
+#ROTA DA PÁGINA DE CADASTRO DE NAVIOS!
+@app.route('/cad')
+def cad():
+    if 'usuario' not in session:
+        return redirect(url_for('login'))
+    return render_template('cad_navios.html', usuario=session['usuario'])
+
 #PÁGINA PARA O USUÁRIO ENTRAR EM CONTATO COM O SUPORTE DA NAVISLOG
 @app.route('/suporte')
 def ajuda():
@@ -65,6 +79,30 @@ def info():
     if 'usuario' not in session:
         return redirect(url_for('login'))
     return render_template('relatorios.html', usurio=session['usuario'])
+
+#ROTA DE ENVIO DO FORMULÁRIO DO SUPORTE
+@app.route('/suporte', methods=['GET'])
+def suporte():
+    return render_template('suporte.html')
+
+@app.route('/enviar-suporte', methods=['POST'])
+def enviar_suporte():
+    nome = request.form.get("nome")
+    email = request.form.get("email")
+    telefone = request.form.get("telefone")
+    assunto = request.form.get("assunto")
+    tipo = request.form.get("tipo")
+    prioridade = request.form.get("prioridade")
+    descricao = request.form.get("descricao")
+    arquivo = request.files.get("arquivo")
+
+    print(nome, email, telefone, assunto, tipo, prioridade, descricao, arquivo)
+    print("Solicitação enviada com sucesso! Aguarde a reposta da equipe de suporte.")
+
+    return render_template("suporte_sucesso.html")
+
+
+
 
 # Rota de logout
 @app.route('/logout')
